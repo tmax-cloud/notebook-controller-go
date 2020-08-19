35f9f3bcb992f0147cc8d51fb33fd8dcd760d062
이승진 <seungjin_lee2@tmax.co.kr>
2020-08-19 14:04:15 +0900

    main.go modified (from kubeflow)

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 /*
-
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,15 +16,14 @@ import (
 	"flag"
 	"os"
 
+	nbv1 "github.com/tmax-cloud/notebook-controller-go/api/v1"
+	"github.com/tmax-cloud/notebook-controller-go/controllers"
+	controller_metrics "github.com/tmax-cloud/notebook-controller-go/pkg/metrics"
 	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	hyperflowv1 "github.com/tmax-cloud/notebook-controller-go/api/v1"
-	"github.com/tmax-cloud/notebook-controller-go/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -38,9 +33,9 @@ var (
 )
 
 func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	_ = clientgoscheme.AddToScheme(scheme)
 
-	utilruntime.Must(hyperflowv1.AddToScheme(scheme))
+	_ = nbv1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -49,18 +44,15 @@ func main() {
 	var enableLeaderElection bool
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
+		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+	ctrl.SetLogger(zap.Logger(true))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
-		Port:               9443,
 		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "0432b6d8.tmax.io",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -68,13 +60,22 @@ func main() {
 	}
 
 	if err = (&controllers.NotebookReconciler{
-		Client: mgr.GetClient(),
-		Log:    ctrl.Log.WithName("controllers").WithName("Notebook"),
-		Scheme: mgr.GetScheme(),
+		Client:        mgr.GetClient(),
+		Log:           ctrl.Log.WithName("controllers").WithName("Notebook"),
+		Scheme:        mgr.GetScheme(),
+		Metrics:       controller_metrics.NewMetrics(mgr.GetClient()),
+		EventRecorder: mgr.GetEventRecorderFor("notebook-controller"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Notebook")
 		os.Exit(1)
 	}
+
+	// uncomment when we need the conversion webhook.
+	// if err = (&nbv1beta1.Notebook{}).SetupWebhookWithManager(mgr); err != nil {
+	// 	setupLog.Error(err, "unable to create webhook", "webhook", "Captain")
+	// 	os.Exit(1)
+	// }
+
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
